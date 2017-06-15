847820bb31461f5519c341448aef1a80d6b41d7a
Rob.Gu <rob.gu@augmentum.com>
2017-06-15 10:16:52 +0800

    base: init wepy demo

---
+++ wepy.config.js
@@ -0,0 +1,54 @@
+var prod = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  wpyExt: '.wpy',
+  eslint: true,
+  compilers: {
+    sass: {
+      outputStyle: 'compressed'
+    },
+    babel: {
+      sourceMap: true,
+      presets: [
+        'es2015',
+        'stage-0'
+      ],
+      plugins: [
+        'transform-decorators-legacy',
+        'transform-export-extensions',
+        'syntax-export-extensions'
+      ]
+    }
+  },
+  plugins: {
+  }
+}
+
+if (prod) {
+  delete module.exports.compilers.babel.sourcesMap
+  // 压缩sass
+  // module.exports.compilers['sass'] = {outputStyle: 'compressed'}
+
+  // sass
+  module.exports.compilers['sass'] = { compress: true }
+
+  // 压缩js
+  module.exports.plugins = {
+    uglifyjs: {
+      filter: /\.js$/,
+      config: {
+      }
+    },
+    imagemin: {
+      filter: /\.(jpg|png|jpeg)$/,
+      config: {
+        jpg: {
+          quality: 80
+        },
+        png: {
+          quality: 80
+        }
+      }
+    }
+  }
+}
